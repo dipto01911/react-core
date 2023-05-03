ce7091b60524e838ff5d8cb15dd65d694c71962f
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-04 00:30:32 +0600

    first project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+function Person(props)
+{
+  let t={
+  height:'200px',
+  width:'30%',
+    margin:'0 auto',
+   border:'2px solid black',
+   backgroundColor:'green',
+   color:'red',
+   textAlign:'center',
+   marginBottom:'10px',
+   
+   
+  }
+  return(
+    <div style={t}>
+      <h1>name:{props.name}</h1>
+      <h2>salary:{props.salary}</h2>
+    </div>
+  )
+}
 
 function App() {
-  return (
+  let x=[{nam:'karim',salary:'1000'},{nam:'zalil',salary:'2000'},{nam:'shamim',salary:'3000'}]
+ return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={x[0].nam} salary={x[0].salary}></Person>
+      <Person name={x[1].nam} salary={x[1].salary}></Person>
+      <Person name={x[2].nam} salary={x[2].salary}></Person>
+
+          </header>
     </div>
   );
 }
