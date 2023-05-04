61a61a6ae61d8758d94191478e936741ebb6c7a6
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-04 12:24:42 +0600

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,53 @@
 
-function Person(props)
-{
+function Person(props){
   let t={
-  height:'200px',
-  width:'30%',
+    height:'200px',
+    width:'30%',
+    border:'2px solid red',
     margin:'0 auto',
-   border:'2px solid black',
-   backgroundColor:'green',
-   color:'red',
-   textAlign:'center',
-   marginBottom:'10px',
-   
-   
+    marginBottom:'30px',
+    textAlign:'center'
   }
   return(
     <div style={t}>
-      <h1>name:{props.name}</h1>
-      <h2>salary:{props.salary}</h2>
+  <h1>Name:{props.name}</h1>
+  <h2>salary:{props.salary}</h2>
     </div>
   )
 }
 
 function App() {
-  let x=[{nam:'karim',salary:'1000'},{nam:'zalil',salary:'2000'},{nam:'shamim',salary:'3000'}]
- return (
+  let k={
+    fontSize:'40px',
+    color:'red',
+    textAlign:'center',
+    listStyle:'none'
+  }
+  let n={
+    fontSize:'50px',
+    color:'green',
+    textAlign:'center',
+    listStyle:'none'
+  }
+  let t=[{name:'karim',roll:'101'},{name:'rahim',roll:'102'},{name:'zalil',roll:'103'}]
+  return (
     <div className="App">
       <header className="App-header">
-      <Person name={x[0].nam} salary={x[0].salary}></Person>
-      <Person name={x[1].nam} salary={x[1].salary}></Person>
-      <Person name={x[2].nam} salary={x[2].salary}></Person>
 
+<Person name={t[0].name} salary={t[0].roll}></Person>
+<Person name={t[1].name} salary={t[1].roll}></Person>
+<Person name={t[2].name} salary={t[2].roll}></Person>
+<ul style={n}>
+  {
+    t.map(x=><li>{x.name}</li>)
+  }
+</ul>
+
+<ul style={k}>
+  {
+    t.map(z=><li>{z.roll}</li>)
+  }
+</ul>
           </header>
     </div>
   );
