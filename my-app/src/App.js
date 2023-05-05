21c58c2f0cb114fa9afd8ee18ea7c26c0aefd55e
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-05 22:56:18 +0600

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,53 +1,82 @@
+import React,{ useEffect, useState } from "react";
 
 function Person(props){
   let t={
-    height:'200px',
-    width:'30%',
+    height:'220px',
+    width:'20%',
+    backgroundColor:'red',
     border:'2px solid red',
     margin:'0 auto',
-    marginBottom:'30px',
-    textAlign:'center'
+    color:'green',
+    textAlign:'center',
+    marginBottom:'20px',
   }
+  
+
   return(
-    <div style={t}>
-  <h1>Name:{props.name}</h1>
-  <h2>salary:{props.salary}</h2>
+    
+   
+   <div style={t}>
+    <h1>{props.head}</h1>
+   <p>this the product </p>
+   <h2>prize:{props.prize}</h2>
+   <button onClick>click me</button>
     </div>
   )
 }
+function Counter(){
+  const[count,setcount]=useState(1);
+const increase=()=>{
+  let newcount =count+1;
+  setcount(newcount);
+}
+return(
+  <div>
+    <h1>count:{count}</h1>
+    <button onClick={increase}>click me</button>
+  </div>
+) 
+}
+// function User(){
+//   const[user,setuser]=useEffect([]);
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res =>res.json())
+//   .then(data => console.log(data));
+// return(
+//   <div>
+//     <p>hello </p>
+//   </div>
+// )
+// }
+//  function User(){
+//   const[user, setuser]=useEffect([]);
+//   useEffect(() => {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res =>res.json())
+//   .then(data => setuser(data));
+//   })
+//   return(
+//     <div>
+//       <h3>number of user:{user.length}</h3>
+//       <ul>
+//         {
+//           user.map(x => <li>{x.name}</li>)
+//         }
+//       </ul> 
+//     </div>
+//   )
+//  }
 
 function App() {
-  let k={
-    fontSize:'40px',
-    color:'red',
-    textAlign:'center',
-    listStyle:'none'
-  }
-  let n={
-    fontSize:'50px',
-    color:'green',
-    textAlign:'center',
-    listStyle:'none'
-  }
-  let t=[{name:'karim',roll:'101'},{name:'rahim',roll:'102'},{name:'zalil',roll:'103'}]
+let t=[{nam:'apple',price:'101'},{nam:'ball',price:'102'},{nam:'cat',price:'103'}]
   return (
     <div className="App">
       <header className="App-header">
-
-<Person name={t[0].name} salary={t[0].roll}></Person>
-<Person name={t[1].name} salary={t[1].roll}></Person>
-<Person name={t[2].name} salary={t[2].roll}></Person>
-<ul style={n}>
-  {
-    t.map(x=><li>{x.name}</li>)
-  }
-</ul>
-
-<ul style={k}>
-  {
-    t.map(z=><li>{z.roll}</li>)
-  }
-</ul>
+       
+       <Counter></Counter>
+        <Person head={t[0].nam} prize={t[0].price}></Person>
+        <Person head={t[1].nam} prize={t[1].price}></Person>
+        <Person head={t[2].nam} prize={t[2].price}></Person>
           </header>
     </div>
   );
