f6a9a7570a269675314f18d2f1a0dc9b368cb050
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-12 08:12:26 +0600

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,40 @@
 import React,{ useEffect, useState } from "react";
 
 
-function Person(props){
+function Person(){
+  const[count,setcount]=useState(0);
   let t={
-    height:'200px',
-    width:'30%',
-    border:'2px solid red',
-    margin:'0 auto',
-    textAlign:'center',
-    marginBottom:'20px',
+    padding:'5px',
+    backgroundColor:'red',
+    borderRadius:'100vmax',
+    marginLeft:'5px',
+    fontSize:'15px'
   }
-  return(
-    <div style={t}>
-      <h1>name:{props.nam} </h1>
-      <h1>salary:{props.sal} </h1>
-    </div>
-  )
+  const inc=()=>{
+   let  newcount=count+1;
+    setcount(newcount);
+  }
+  const dec=()=>{
+    let newcount=count-1;
+    setcount(newcount);
+  }
+return(
+  <div>
+    <h1>count:{count}</h1>
+    <button onClick={inc} style={t}>increase</button>
+    <button onClick={dec} style={t}>decrease</button>
+  </div>
+)
 }
 
+
 function App() {
-  let ara=[{name:'karinm',roll:'101'},{name:'nazim',roll:'102'},{name:'zasim',roll:'103'}];
+  
 
 return (
     <div className="App">
       <header className="App-header">
-       <Person nam={ara[0].name} sal={ara[0].roll}></Person>
-       <Person nam={ara[1].name} sal={ara[1].roll}></Person>
-       <Person nam={ara[2].name} sal={ara[2].roll}></Person>
+       <Person></Person>
  
           </header>
     </div>
