dfb14ee294f8ce15b62568dabd51a6b09b5664c4
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-12 13:55:18 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # use-state
+# use-state
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,68 @@
 import React,{ useEffect, useState } from "react";
 
 
-function Person(){
-  const[count,setcount]=useState(0);
-  let t={
-    padding:'5px',
-    backgroundColor:'red',
-    borderRadius:'100vmax',
-    marginLeft:'5px',
-    fontSize:'15px'
-  }
-  const inc=()=>{
-   let  newcount=count+1;
-    setcount(newcount);
-  }
-  const dec=()=>{
-    let newcount=count-1;
-    setcount(newcount);
-  }
+
+  function Counter(){
+    let x={
+      padding:'5px',
+      backgroundColor:'red',
+      color:'green',
+      borderRadius:'100vmax',
+      marginLeft:'10px',
+      fontSize:'20px'
+    }
+    const [count,setcount]=useState(0);
+    const inc=()=>{
+      let newcount=count+1;
+      setcount(newcount);
+    }
+    const dec=()=>{
+      let newcount=count-1;
+      setcount(newcount);
+    }
 return(
   <div>
     <h1>count:{count}</h1>
-    <button onClick={inc} style={t}>increase</button>
-    <button onClick={dec} style={t}>decrease</button>
+    <button onClick={inc} style={x}>increase</button>
+    <button onClick={dec} style={x}>decrease</button>
+
+  </div>
+)
+  }
+
+  function User(){
+    let x1={
+      fontSize:'25px',
+      color:'green',
+      fontStyle:'italic'
+    }
+ const[user,setuser]=useState([]);
+ useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res=>res.json())
+  .then(data=>setuser(data))
+ })
+  
+
+return(
+  <div>
+   <li>
+    {
+      user.map(x=><ul style={x1}>{x.name}</ul>)
+    }
+   </li>
   </div>
 )
 }
-
-
 function App() {
   
 
 return (
     <div className="App">
       <header className="App-header">
-       <Person></Person>
- 
+    <Counter></Counter>
+    <User></User>
+
           </header>
     </div>
   );
