a4067e41f118c720a827712a8754bc4284ace0d4
dipto 0191101~ <diptoghoshbdsatkhira@gmail.com>
2023-05-14 12:35:39 +0600

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,69 +1,54 @@
+
 import React,{ useEffect, useState } from "react";
 
+function User(){
+  
+  const[user,setuser]=useState([])
 
+  useEffect(()=>{
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(res => res.json())
+   .then(data => setuser(data))
+ 
+  })
+  
 
-  function Counter(){
-    let x={
-      padding:'5px',
-      backgroundColor:'red',
-      color:'green',
-      borderRadius:'100vmax',
-      marginLeft:'10px',
-      fontSize:'20px'
-    }
-    const [count,setcount]=useState(0);
-    const inc=()=>{
-      let newcount=count+1;
-      setcount(newcount);
-    }
-    const dec=()=>{
-      let newcount=count-1;
-      setcount(newcount);
-    }
-return(
-  <div>
-    <h1>count:{count}</h1>
-    <button onClick={inc} style={x}>increase</button>
-    <button onClick={dec} style={x}>decrease</button>
-
-  </div>
-)
+  return(
+<div>
+  {
+    user.map(x => <Person nam={x.name} jsx={x.username} ></Person>)
   }
-
-  function User(){
-    let x1={
-      fontSize:'25px',
-      color:'green',
-      fontStyle:'italic'
-    }
- const[user,setuser]=useState([]);
- useEffect(()=>{
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res=>res.json())
-  .then(data=>setuser(data))
- })
-  
-
-return(
-  <div>
-   <li>
-    {
-      user.map(x=><ul style={x1}>{x.name}</ul>)
-    }
-   </li>
-  </div>
-)
+</div>
+  )
 }
-function App() {
-  
 
+function Person(props){
+  let t={
+    height:'150px',
+    width:'30%',
+    margin:'0 auto',
+    marginBottom:'10px',
+    border:'2px solid  red',
+    backgroundColor:'green',
+    textAlign:'center',
+    fontSize:'10px',
+  }
+  return(
+    <div style={t}>
+      <h1>name :{props.nam}</h1>
+      <h2>username:{props.jsx}</h2>
+    </div>
+  )
+}
+
+function App(){
+ 
 return (
     <div className="App">
       <header className="App-header">
-    <Counter></Counter>
-    <User></User>
+        <User></User>
 
-          </header>
+       </header>
     </div>
   );
 }
